a960f6f9fa4711a3f66baf7f936d2be6d5de27f8
Rob Rose <RobRoseKnows@gmail.com>
2018-05-08 10:19:41 -0400

    Set up stuff for original testing.

---
+++ tests/__main__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, print_function
+
+import unittest
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+from .test_grammars import(
+
+
+    )
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_grammars.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, print_function
+
+import unittest
+import logging
+import os
+import sys
+

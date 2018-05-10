ce4a7b685ed3b078b8893b7fd77512b144c21344
Rob Rose <RobRoseKnows@gmail.com>
2018-05-10 17:28:41 -0400

    Added more to common.py

--- download-game-files/common.py
+++ download-game-files/common.py
@@ -1,1 +1,2 @@
-FOLDERS = ["common", "events", "localisation", "localisation_synced", "prescripted_countries"]
+FOLDERS = ["common", "events", "localisation", "localisation_synced", "map", "prescripted_countries"]
+VERSIONS = ["1.9.1", "2.0.1", "2.0.2", "2.0.4"]

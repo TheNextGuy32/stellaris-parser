c1fae5610f3a24fe2760f261f007a067df150615
Rob Rose <RobRoseKnows@gmail.com>
2018-05-12 17:06:38 -0400

    Worked on AWS Tools some more
    - Added the constants to common.py for the folders and versions.
    - Added a bunch of modifications to upload.py in order to make it
      testable via unit tests.
    - Might have also done a lot of the logic in upload.py, don't remember
      my last commit.
    - Also added future imports to upload.py in order to make sure it works
      in Python 2.7.

--- aws-tools/common.py
+++ aws-tools/common.py
@@ -1,2 +1,2 @@
-folders = ["common", "events", "localisation", "localisation_synced", "map", "prescripted_countries"]
-versions = ["2.0.1"]
+folders = frozenset(["common", "events", "localisation", "localisation_synced", "map", "prescripted_countries"])
+versions = frozenset(["2.0.1"])
--- aws-tools/upload.py
+++ aws-tools/upload.py
@@ -1,12 +1,49 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
 import os
 import sys
 import boto3
 
 from common import folders, versions
 
-session = boto3.Session(
-    aws_access_key_id = os.getenv("AWS_ACCESS_KEY"),
-    aws_secret_access_key = os.getenv("AWS_SECRET_KEY"),
-    region_name="us-east-1")
+def main():
 
-s3 = session.resource('s3')
+    def setup_s3(bucket_name):
+        # Returns session, s3 and bucket
+        session = boto3.Session(
+            aws_access_key_id = os.getenv("AWS_ACCESS_KEY"),
+            aws_secret_access_key = os.getenv("AWS_SECRET_KEY"),
+            region_name="us-east-1")
+
+        s3 = session.resource('s3')
+        bucket = s3.Bucket(bucket_name)
+
+        return session, s3, bucket
+
+    def get_args():
+        local_directory = None
+        bucket_name = None
+        destination = None
+        if len(sys.argv) == 4:
+            local_directory, bucket_name, destination = sys.argv[1:4]
+        elif len(sys.argv) == 2:
+            local_directory = sys.argv[1]
+        return local_directory, bucket_name, destination
+
+    def remove_unwanted(dirs):
+        dirs[:] = [d for d in dirs if d in folders]
+
+    local_directory, bucket_name, destination = get_args()
+    if bucket_name is not None:
+        session, s3, bucket = setup_s3(bucket_name)
+    else:
+        session, s3, bucket = None, None, None
+
+    for root, dirs, files in os.walk(local_directory):
+        if os.path.basename(root) in versions:
+            remove_unwanted(dirs)
+
+        print(root, dirs, files)
+
+
+main()

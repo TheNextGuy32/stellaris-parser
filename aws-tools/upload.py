c977cc37b596b9b9e02fe41ee3e2e2d2725d5ba2
Rob Rose <RobRoseKnows@gmail.com>
2018-05-12 16:21:52 -0400

    Added pycache to ignore and worked on S3 upload.

--- aws-tools/upload.py
+++ aws-tools/upload.py
@@ -1,1 +1,12 @@
+import os
+import sys
+import boto3
+
 from common import folders, versions
+
+session = boto3.Session(
+    aws_access_key_id = os.getenv("AWS_ACCESS_KEY"),
+    aws_secret_access_key = os.getenv("AWS_SECRET_KEY"),
+    region_name="us-east-1")
+
+s3 = session.resource('s3')

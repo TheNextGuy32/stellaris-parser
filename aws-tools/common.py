ced55fda0b6c50761436abddff31996ea76a06da
Rob Rose <RobRoseKnows@gmail.com>
2018-05-12 12:37:21 -0400

    Added new aws-tools folder.

---
+++ aws-tools/common.py
@@ -0,0 +1,2 @@
+folders = ["common", "events", "localisation", "localisation_synced", "map", "prescripted_countries"]
+versions = ["2.0.1"]
--- download-game-files/common.py
+++
@@ -1,2 +0,0 @@
-FOLDERS = ["common", "events", "localisation", "localisation_synced", "map", "prescripted_countries"]
-VERSIONS = ["1.9.1", "2.0.1", "2.0.2", "2.0.4"]

534cd1a208a563626b2b1ae853b5b2d756c9d8bb
Rob Rose <RobRoseKnows@gmail.com>
2018-07-09 19:06:39 -0700

    Added better download stuff.

--- stellaris_parser/aws_tools/download.py
+++ stellaris_parser/aws_tools/download.py
@@ -6,6 +6,19 @@ import boto3
 
 from common import folders, versions
 
+# Credit: https://stackoverflow.com/a/33350380/1021259
+def download_dir(client, resource, dist, local='/tmp', bucket='your_bucket'):
+    paginator = client.get_paginator('list_objects')
+    for result in paginator.paginate(Bucket=bucket, Delimiter='/', Prefix=dist):
+        if result.get('CommonPrefixes') is not None:
+            for subdir in result.get('CommonPrefixes'):
+                download_dir(client, resource, subdir.get('Prefix'), local, bucket)
+        if result.get('Contents') is not None:
+            for file in result.get('Contents'):
+                if not os.path.exists(os.path.dirname(local + os.sep + file.get('Key'))):
+                     os.makedirs(os.path.dirname(local + os.sep + file.get('Key')))
+                resource.meta.client.download_file(bucket, file.get('Key'), local + os.sep + file.get('Key'))
+
 def main(local_directory, bucket_name):
 
     def setup_s3(bucket_name):
@@ -22,22 +35,14 @@ def main(local_directory, bucket_name):
 
         return session, s3, bucket
 
-    def create_folder_if_dne(key, root):
-        directory = os.path.dirname(key)
-        # No need to do recursive calls because of makedirs
-        full_name = os.path.normpath(os.path.join(root, directory))
-        if not os.path.exists(full_name):
-            os.makedirs(full_name)
-
     if bucket_name is not None:
         session, s3, bucket = setup_s3(bucket_name)
     else:
         session, s3, bucket = None, None, None
 
-    for key in bucket.objects.all():
-        create_folder_if_dne(key.key, local_directory)
-        bucket.download_file(key.key, os.path.normpath(os.path.join(local_directory, key.key)))
-        print("{} downloaded to {}".format("s3://{}/{}".format(bucket_name, key.key), os.path.normpath(os.path.join(local_directory, key.key))))
+    client = session.client('s3')
+    download_dir(client, s3, '/', local=local_directory, bucket=bucket_name)
+
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2])

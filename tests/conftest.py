504b0eed351835a12f42bb5416502e6a5b4304af
Rob Rose <RobRoseKnows@gmail.com>
2018-07-09 19:29:36 -0700

    Working on tests again.

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import os
+
+import pytest
+
+from stellaris_parser.aws_tools import download
+
+@pytest.fixture(params=['1.8.3', '1.9.1', '2.0.1', '2.0.5', '2.1.0'], scope="session")
+def game_files_path(request):
+    this_ver = os.path.abspath(os.path.join('../game_files', request.param))
+    if not os.path.isdir(this_ver):
+        # Just use default bucket name.
+        download.main(this_ver, remote=os.path.join('game_files', request.param))
+    return this_ver
